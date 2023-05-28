3de14bc2195200acd2f7ef484bdc0fc9306e6083
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2023-05-27 23:41:37 -0700

    Add button interactivity

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const buttons = document.querySelectorAll(".button");
+    buttons.forEach((button) => {
+        button.addEventListener("mousedown", () => {
+            button.style.backgroundColor = "lightgrey";
+        });
+        button.addEventListener("mouseup", () => {
+            button.style.backgroundColor = "grey";
+        });
+        button.addEventListener("click", () => {
+            const currentEquation = document.querySelector("#current-equation");
+            currentEquation.innerHTML = 12;
+        });
+    });
+
+
 });
